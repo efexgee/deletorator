71f25b93d11d202195950114f6350fa04f4b3213
Falko Goettsch <falko@uw.edu>
2017-08-08 13:11:59 -0700

    removed random script; commented script

--- deletorator.sh
+++ deletorator.sh
@@ -1,61 +1,70 @@
 #/bin/bash
 
-# Intended to run as multiple instances of this
+# Deletes directories or files placed into the current directory while avoiding
+# multiple deletes working on the same directory tree
+
+# Intended to run as multiple instances from the command line, in the background
+# Ideally it would be a single worker spawning processes
 # Does not clean up temp dirs when it's killed
-# Does not have a clean way to exit
 
 umask 022
 
-# this is script is scary and will only run inside a dir named this:
+# Because this script is scary will only run inside a dir named this:
+# (These directories are created by my 'tombstone' alias
 CHECK_NAME="TO_BE_DELETED"
-SUB_DIR="DELETORATOR"
-# using our parent's PID so it matches the jobs -p -l output
-PID=$PPID
-TEMP_DIR="$SUB_DIR/$PID"
-STOP_FILE="$TEMP_DIR/STOP_DELETORATOR"
-STOP_ALL_FILE="$SUB_DIR/STOP_ALL_DELETORATORS"
-ME="[$PID]"
-# random start delay in case many deletorators are started simultaneously
-START_JITTER=5  # seconds
-# how long to wait if the dir is empty and how much random wait to add to
-# reduce the likelihood of collisions
-WAIT=300    # seconds
-WAIT_JITTER=60  # seconds
 
+# Define deletorator working directories
+SUB_DIR="DELETORATOR"
+PID=$PPID                                       # parent pid so it matches jobs -p -l output
+TEMP_DIR="$SUB_DIR/$PID"                        # deletorator's working dir
+
+# Define files to stop deletorators
+# deletorators only check when they are looking for a new task
+# This will not interrupt them
+STOP_FILE="$TEMP_DIR/STOP_DELETORATOR"          # to stop a single deletorator
+STOP_ALL_FILE="$SUB_DIR/STOP_ALL_DELETORATORS"  # to stop all deletorators
+
+# Prefix for messages to identify who printed it
+ME="[$PID]"
+
+# Define timeouts
+WAIT=300        # seconds to wait if there is nothing to delete
+WAIT_JITTER=60  # wait up to this many additional seconds to avoid collisions
+START_JITTER=5  # wait up to this many seconds before starting the first deletion
+
+# grab current dir name
 path=`pwd`
 cur_dir=`basename $path`
 
-# safety check
+# check that we're in a dir with the correct name to avoid disasters
 if [ $cur_dir != $CHECK_NAME ]; then
 	echo "$ME The current directory ($path) doesn't match the expected name: $CHECK_NAME"
 	exit 2
 fi
 
-# create this worker's temp directory
+# create this worker's temp directory (including parents)
 mkdir -p $TEMP_DIR
 
 if [ $? == 0 ]; then
+    #XXX probably too verbose
     echo "$ME Created temp dir: $TEMP_DIR"
 else
     echo "$ME Failed to create temp dir: $TEMP_DIR. Exiting."
     exit 3
 fi
 
-# delay start
-# do icky trickery to make random floating point number
+# delay initial start
+# (icky string trickery to make random floating point number)
 start_jitter=$(($START_JITTER * 10))
 start_wait=`echo $((RANDOM % $start_jitter + 1 )) | sed 's/\([0-9]\)$/.\1/'`
-
 echo "$ME Waiting $start_wait seconds to start."
-
 sleep $start_wait
 
 echo "$ME Starting. To stop me, create the file '$STOP_FILE'"
 
 # main loop
 while true; do
-	#echo "$ME Entering main loop."
-
+    # check for stop files
     if [ -f $STOP_FILE ] || [ -f $STOP_ALL_FILE ]; then
 
         if [ -f $STOP_FILE ]; then
@@ -68,17 +77,21 @@ while true; do
         fi
 
         echo "$ME Exiting because stop file found: $stop_file"
+        #XXX this seems to fail regularly (always?)
         rmdir $TEMP_DIR
         exit 0
     fi
 
-	# exclude the temp dirs and stop file from the deletions
+	# exclude the temp dirs and the stop file from the deletions
+    # and pick a random entry to delete
 	target=`ls | grep -vi deletorator | sort -R | head -1`
 	
-	# check the dir for things to delete
+	# check whether we got something to delete
+    # this step is subject to race conditions but I think it's unlikely
 	if [ -n "$target" ]; then
 		# delete
 		echo "$ME Targeting: $target"
+        # move the directory to our working dir so nobody else grabs it
 		mv $target $TEMP_DIR/ || echo "$ME Failed to move file to temp dir ($TEMP_DIR): $target"
 		rm -r --one-file-system --interactive=never $TEMP_DIR/$target || echo "$ME Failed to delete: $TEMP_DIR/$target"
 	else
@@ -91,5 +104,6 @@ while true; do
 	fi
 done
 
-# without a clean way to exit, the script should never get here
+# the script either gets ctrl-c'd or stops because of a stop file
+# so it should never get here
 echo "$ME Exited main loop. This should never happen."
